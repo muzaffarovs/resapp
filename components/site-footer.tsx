2cdedf74e4fd4a30a9a21967768c44b55f36a459
Rajab Muzaffarov <muzaffarovrajab25@gmail.com>
2025-02-12 23:37:16 +0400

    fix: remove unused imports

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 import React from "react";
 
 const SiteFooter = ({ className = "" }: { className?: string }) => {
-  const { activeTab, setActiveTab } = useTab();
+  const { setActiveTab } = useTab();
   return (
     <div
       className={`px-[2rem] md:mb-1 md:px-6 fixed w-full md:w-[calc(100%-4rem)] max-w-7xl bottom-0 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-gray-800 via-gray-900 to-black bg-opacity-80 backdrop-blur-lg py-4 md:rounded-xl border border-gray-700 shadow-2xl text-white ${className}`}
